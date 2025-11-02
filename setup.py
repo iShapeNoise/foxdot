96a5fd87a63449e4991139cccb79f428c34d3239
iShapeNoise <contact@jensmeisner.net>
2025-11-02 13:43:36 +0100

    Update 021125

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 with open("README.md", "r") as f:
-    long_description=f.read()
+    long_description = f.read()
 
 with open("FoxDot/lib/.version", "r") as f:
     version = f.read()
